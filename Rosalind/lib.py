3406318706ddbb7a842b7efdad742a2759dfb8e4
Maria <mariarosekesa@gmail.com>
2023-12-11 21:52:57 +0200

    gitignore

--- Rosalind/lib.py
+++ Rosalind/lib.py
@@ -1,6 +1,12 @@
 
+def BA1B_data_parsing(text):
+    '''
+    Most frequent kmer
+    '''
+    print(find_most_frequent_kmer.__name__)
+    return 0
 
-def BA1B_function(text):
+def BA1B_algorithm(text):
     '''
     Most frequent kmer
     '''
